e3baaa84de5ae3acff759ac9cf9ac47af6cd658d
Min Ko Naing <mkn.minko.naing99@gmail.com>
2025-07-19 11:41:20 +0700

    starting now

--- api/dbinfo.php
+++ api/dbinfo.php
@@ -1,9 +1,14 @@
 <?php
-$host = 'localhost';
-$port = 3307;
+// $host = 'localhost';
+// $port = 3307;
+// $dbname = 'digimium';
+// $username = 'root';
+// $password = 'Tkhantnaing1';
+$host = 'digimium.cxgik26kid7z.ap-southeast-1.rds.amazonaws.com';
+$username = 'admin';
+$port = 3306;
+$password = 'Tkhantnaing1'; // replace with your RDS password
 $dbname = 'digimium';
-$username = 'root';
-$password = 'Tkhantnaing1';
 
 try {
     $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4", $username, $password);
